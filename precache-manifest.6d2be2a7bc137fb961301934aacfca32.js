b02374ebb57dd5118c5b160859b2cb03725ace7f
Zachary Sutton <zachary-sutton@outlook.com>
2020-03-15 21:39:27 +0000

    Updates

---
+++ precache-manifest.6d2be2a7bc137fb961301934aacfca32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381d2bfa7d7e58cfe037de9534642a82",
+    "url": "/ecommerce-dashboard/index.html"
+  },
+  {
+    "revision": "6111e34236db87ae7c9a",
+    "url": "/ecommerce-dashboard/static/css/main.4914ad58.chunk.css"
+  },
+  {
+    "revision": "c101907fbfeae842fec9",
+    "url": "/ecommerce-dashboard/static/js/2.9668ab4e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ecommerce-dashboard/static/js/2.9668ab4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6111e34236db87ae7c9a",
+    "url": "/ecommerce-dashboard/static/js/main.b02a79fa.chunk.js"
+  },
+  {
+    "revision": "612ea970f60eb143b8a5",
+    "url": "/ecommerce-dashboard/static/js/runtime-main.9c759be9.js"
+  }
+]);
